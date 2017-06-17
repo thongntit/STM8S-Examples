a2f8f1af103dd5e1322d1e9dfe1c1cd4e4e66415
nanolove95 <nanolove95@gmail.com>
2017-06-18 05:55:16 +0700

    STM8S Tutorial

--- 4-TIM/STM8S_StdPeriph_Driver/main.c
+++ 4-TIM/STM8S_StdPeriph_Driver/main.c
@@ -2,8 +2,17 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm8s.h"
 
+u16 count=0;
+static void TIM4_Config(void); 
 void main(void)
 {
+  /*Clock Config */
+  CLK_DeInit();
+  CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
+  while(CLK_GetFlagStatus(CLK_FLAG_HSIRDY)!=SET);
+  /* GPIO Config*/
+  GPIO_Init(GPIOB,GPIO_PIN_5,GPIO_MODE_OUT_PP_LOW_FAST); //Led Pin
+  TIM4_Config();
   /* Infinite loop */
   while (1)
   {
@@ -11,6 +20,39 @@ void main(void)
   
 }
 
+INTERRUPT_HANDLER(TIM4_UPD_OVF_IRQHandler, 23)
+{
+  count++;
+  if(count==1000)
+  {
+    GPIO_WriteReverse(GPIOB,GPIO_PIN_5);
+  }
+}
+
+static void TIM4_Config(void)
+{
+  /* TIM4 configuration:
+   - TIM4CLK is set to 16 MHz, the TIM4 Prescaler is equal to 128 so the TIM1 counter
+   clock used is 16 MHz / 128 = 125 000 Hz
+  - With 125 000 Hz we can generate time base:
+      max time base is 2.048 ms if TIM4_PERIOD = 255 --> (255 + 1) / 125000 = 2.048 ms
+      min time base is 0.016 ms if TIM4_PERIOD = 1   --> (  1 + 1) / 125000 = 0.016 ms
+  - In this example we need to generate a time base equal to 1 ms
+   so TIM4_PERIOD = (0.001 * 125000 - 1) = 124 */
+
+  /* Time base configuration */
+  TIM4_TimeBaseInit(TIM4_PRESCALER_128, 124);
+  /* Clear TIM4 update flag */
+  TIM4_ClearFlag(TIM4_FLAG_UPDATE);
+  /* Enable update interrupt */
+  TIM4_ITConfig(TIM4_IT_UPDATE, ENABLE);
+  
+  /* enable interrupts */
+  enableInterrupts();
+
+  /* Enable TIM4 */
+  TIM4_Cmd(ENABLE);
+}
 
 #ifdef USE_FULL_ASSERT
 
